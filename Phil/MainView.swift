1674eda37d8a49e03ea52d235a24e70085ae3418
Lau <hi@lau.work>
2023-02-12 15:39:26 -0300

    refactor: use SwiftUI instead of UIKit

---
+++ Phil/MainView.swift
@@ -0,0 +1,20 @@
+//
+//  MainView.swift
+//  Phil
+//
+//  Created by Lau on 12-02-23.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World! I'm Phil :D")
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ Phil/PhilApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhilApp.swift
+//  Phil
+//
+//  Created by Lau on 12-02-23.
+//
+
+import SwiftUI
+
+@main
+struct PhilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
